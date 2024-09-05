e2f943a3cf286d2212c4e704cf38aa59f013c603
daisychen <daisyc2027@student.cis.edu.hk>
2024-09-05 13:43:35 +0800

    done second lab

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: DAISY CHEN
 *****************************************************************************************************
 */
 
@@ -11,21 +11,40 @@ public class Main {
     public static void main(String[] args) {
 
     }
-
     // 1. add
-    
+    public static int add(int a, int b){
+        return a + b;
+    }
     // 2. add
-
+    public static int add(int a, int b, int c, int d){
+        return a + b + c + d;
+    }
     // 3. morningGreeting
-
+    public static String morningGreeting(String name){
+        return "早上好, " + name +"!";
+    }
     // 4. afternoonGreeting
-
+    public static String afternoonGreeting(String name){
+        return "下午好, " + name+"!";
+    }
     // 5. triple
+    public static String triple(String randomWord){
+        return randomWord + randomWord + randomWord;
+    }
 
     // 6. half
-
+    public static double half(int a){
+        return (double) a / 2;
+    }
     // 7. roundPositiveValueToNearestInteger
-
+    public static double roundPositiveValueToNearestInteger(double a){
+        a += 0.5;
+        return (int) a;
+    }
     // 8. roundNegativeValueToNearestInteger
+    public static double roundNegativeValueToNearestInteger(double a){
+        a -= 0.5;
+        return (int) a;
+    }
 
 }
